65a3982154db518038dba51fc291292c895bbb89
VictoriaT87 <victoriaemt@gmail.com>
2023-02-08 12:02:02 +0000

    add testing for mocking

--- students.py
+++ students.py
@@ -1,8 +1,9 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
-    """A Student class as base method for testing"""
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -14,3 +15,21 @@ class Student:
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "something went wrong with the request!"
